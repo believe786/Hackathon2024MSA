eec015aa50dc28f412c236539ac359e6f7bccf85
believe786 <50620555+believe786@users.noreply.github.com>
2024-06-27 09:30:37 +0000

    Added Graph

--- app.py
+++ app.py
@@ -5,7 +5,23 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  # Define Plot Data 
+  labels = [
+      'Monday',
+      'Tuesday',
+      'Wednesday',
+      'Thursday',
+      'Friday',
+      'Saturday',
+      'Sunday',
+  ]
+
+  data = [6, 7, 8, 8, 7, 4, 5]
+  return render_template(
+    template_name_or_list='home.html',
+    data=data,
+    labels=labels,
+    )
 
 
 if __name__ == '__main__':
