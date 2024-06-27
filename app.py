1c9b92e2da1fb0649b3140f4964310b418ecb20a
believe786 <50620555+believe786@users.noreply.github.com>
2024-06-27 16:54:02 +0000

    Chart added

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template
+import pandas as pd
+import json
+import plotly
+import plotly.express as px
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
+def monitor_my_stress():
   # Define Plot Data 
   labels = [
       'Monday',
@@ -21,6 +25,28 @@ def hello_world():
     template_name_or_list='home.html',
     data=data,
     labels=labels,
+    )
+
+@app.route("/analysis")
+def analysisOfStressLevel():
+  # Define Plot Data 
+  # emotionsData = [['Happy', 3],
+  #   ['Neutral', 4],
+  #   ['Sad', 2],
+  #   ['Anxious/Nervous', 5]]
+
+  # # Convert list to dataframe and assign column values
+  # emotionsDataDf = pd.DataFrame(emotionsData,
+  #   columns=['Emotions','Frequeny'],
+  #   index=['a', 'b', 'c', 'd'])
+  # # Create Bar chart
+  # fig = px.bar(emotionsDataDf, x='Emotions', y='Frequeny', barmode='group')
+
+  # # Create graphJSON
+  # graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+
+  return render_template(
+    template_name_or_list='analysisChart.html'
     )
 
 
